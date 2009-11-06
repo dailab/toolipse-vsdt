f162930d9705731d11f24ee7a51a6d21c248cd7c
kuester <kuester@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2009-11-06 12:21:13 +0000

    removing obsolete features (still present in old location jiac/vsdt and in branches/tags)
    git-svn-id: https://cvs.dai-labor.de/jiac/toolipse3/trunk@25286 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- de.dailab.vsdt.diagram_gmf20/src/de/dailab/vsdt/diagram/edit/helpers/IntermediateEditHelper.java
+++
@@ -1,7 +0,0 @@
-package de.dailab.vsdt.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class IntermediateEditHelper extends VsdtBaseEditHelper {
-}
--- de.dailab.vsdt.diagram_gmf20/src/de/dailab/vsdt/diagram/edit/helpers/StartEditHelper.java
+++
@@ -1,7 +0,0 @@
-package de.dailab.vsdt.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class StartEditHelper extends VsdtBaseEditHelper {
-}
