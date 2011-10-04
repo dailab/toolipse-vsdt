454d4e05721b95144d1956846bb014c723cdcda5
pstan <pstan@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2011-10-04 10:24:49 +0000

    git-svn-id: https://cvs.dai-labor.de/jiac/toolipse3/trunk@34262 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- de.dailab.vsdt.trafo.jiacbeans/src/de/dailab/vsdt/trafo/jiacbeans/export/generated/AgentBeanGenerator.java
+++ de.dailab.vsdt.trafo.jiacbeans/src/de/dailab/vsdt/trafo/jiacbeans/export/generated/AgentBeanGenerator.java
@@ -19,32 +19,31 @@ public class AgentBeanGenerator
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "";
-  protected final String TEXT_2 = NL;
-  protected final String TEXT_3 = NL + NL + "import de.dailab.jiactng.agentcore.action.AbstractMethodExposingBean;" + NL + "import de.dailab.jiactng.agentcore.action.scope.ActionScope;";
-  protected final String TEXT_4 = NL + "import ";
-  protected final String TEXT_5 = ";";
+  protected final String TEXT_2 = NL + NL + "import de.dailab.jiactng.agentcore.action.AbstractMethodExposingBean;" + NL + "import de.dailab.jiactng.agentcore.action.scope.ActionScope;";
+  protected final String TEXT_3 = NL + "import ";
+  protected final String TEXT_4 = ";";
+  protected final String TEXT_5 = NL;
   protected final String TEXT_6 = NL;
-  protected final String TEXT_7 = NL;
-  protected final String TEXT_8 = "{";
-  protected final String TEXT_9 = NL + "\tpublic final static String ";
-  protected final String TEXT_10 = " = \"";
-  protected final String TEXT_11 = "\"; ";
-  protected final String TEXT_12 = NL;
-  protected final String TEXT_13 = NL + "\t";
-  protected final String TEXT_14 = ";";
-  protected final String TEXT_15 = NL;
-  protected final String TEXT_16 = NL + "\t/**" + NL + "\t *  delete the generated tag after you edited this method" + NL + "\t *  @generated" + NL + "\t */" + NL + "\t";
-  protected final String TEXT_17 = NL + "\t@Expose(name = ";
-  protected final String TEXT_18 = ", scope = ActionScope.GLOBAL)" + NL + "\t";
-  protected final String TEXT_19 = NL + "\t";
-  protected final String TEXT_20 = NL;
-  protected final String TEXT_21 = " ";
+  protected final String TEXT_7 = "{";
+  protected final String TEXT_8 = NL + "\tpublic final static String ";
+  protected final String TEXT_9 = " = \"";
+  protected final String TEXT_10 = "\"; ";
+  protected final String TEXT_11 = NL;
+  protected final String TEXT_12 = NL + "\t";
+  protected final String TEXT_13 = ";";
+  protected final String TEXT_14 = NL;
+  protected final String TEXT_15 = NL + "\t/**" + NL + "\t *  delete the generated tag after you edited this method" + NL + "\t *  @generated" + NL + "\t */" + NL + "\t";
+  protected final String TEXT_16 = NL + "\t@Expose(name = ";
+  protected final String TEXT_17 = ", scope = ActionScope.GLOBAL)" + NL + "\t";
+  protected final String TEXT_18 = NL + "\t";
+  protected final String TEXT_19 = NL;
+  protected final String TEXT_20 = " ";
+  protected final String TEXT_21 = NL;
   protected final String TEXT_22 = NL;
   protected final String TEXT_23 = NL;
   protected final String TEXT_24 = NL;
   protected final String TEXT_25 = NL;
-  protected final String TEXT_26 = NL;
-  protected final String TEXT_27 = NL + "}";
+  protected final String TEXT_26 = NL + "}";
 
   public String generate(Object argument)
   {
@@ -67,41 +66,40 @@ public class AgentBeanGenerator
    SubProcessGenerator spg = new SubProcessGenerator();
 
     stringBuffer.append(TEXT_1);
-    stringBuffer.append(TEXT_2);
     stringBuffer.append(packageDeclaration);
-    stringBuffer.append(TEXT_3);
+    stringBuffer.append(TEXT_2);
      for(String i : imports){
-    stringBuffer.append(TEXT_4);
+    stringBuffer.append(TEXT_3);
     stringBuffer.append(i);
+    stringBuffer.append(TEXT_4);
+    }
     stringBuffer.append(TEXT_5);
-    }
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(TEXT_7);
     stringBuffer.append(classDeclaration);
-    stringBuffer.append(TEXT_8);
+    stringBuffer.append(TEXT_7);
      for(Action act: actions){
-    stringBuffer.append(TEXT_9);
+    stringBuffer.append(TEXT_8);
     stringBuffer.append( act.getName().toUpperCase());
-    stringBuffer.append(TEXT_10);
+    stringBuffer.append(TEXT_9);
     stringBuffer.append( act.getLocation());
+    stringBuffer.append(TEXT_10);
+    }
     stringBuffer.append(TEXT_11);
-    }
-    stringBuffer.append(TEXT_12);
      for(JavaVariable var: attributes){
-    stringBuffer.append(TEXT_13);
+    stringBuffer.append(TEXT_12);
     stringBuffer.append( var.toString());
-    stringBuffer.append(TEXT_14);
+    stringBuffer.append(TEXT_13);
     }
-    stringBuffer.append(TEXT_15);
+    stringBuffer.append(TEXT_14);
      for(Method method : methods){
-    stringBuffer.append(TEXT_16);
+    stringBuffer.append(TEXT_15);
     Action act = actionMap.get(method.getName());
 	if(act!=null){
-    stringBuffer.append(TEXT_17);
+    stringBuffer.append(TEXT_16);
     stringBuffer.append(act.getName());
-    stringBuffer.append(TEXT_18);
+    stringBuffer.append(TEXT_17);
     }
-    stringBuffer.append(TEXT_19);
+    stringBuffer.append(TEXT_18);
     String cS = method.toString();
 	  String result = "";
 	  BufferedReader reader = new BufferedReader(new StringReader(cS));
@@ -113,11 +111,11 @@ public class AgentBeanGenerator
 		}
 	  }catch(IOException e){}
 	
-    stringBuffer.append(TEXT_20);
+    stringBuffer.append(TEXT_19);
     stringBuffer.append( result+"\n");
-    stringBuffer.append(TEXT_21);
+    stringBuffer.append(TEXT_20);
     }
-    stringBuffer.append(TEXT_22);
+    stringBuffer.append(TEXT_21);
      for(SubProcess sub : subprocesses){
     String cS = spg.generate(sub);
 	  String result = "";
@@ -130,10 +128,10 @@ public class AgentBeanGenerator
 		}
 	  }catch(IOException e){}
 	
-    stringBuffer.append(TEXT_23);
+    stringBuffer.append(TEXT_22);
     stringBuffer.append( result+"\n");
     }
-    stringBuffer.append(TEXT_24);
+    stringBuffer.append(TEXT_23);
      if(bean.isHandlingTimeoutEvent()){
     String cS = new TimeoutEventHandlerGenerator().generate(null);
 	  String result = "";
@@ -146,7 +144,7 @@ public class AgentBeanGenerator
 		}
 	  }catch(IOException e){}
 	
-    stringBuffer.append(TEXT_25);
+    stringBuffer.append(TEXT_24);
     stringBuffer.append( result+"\n");
     }
      if(bean.isHandlingMessageEvent()){
@@ -161,10 +159,10 @@ public class AgentBeanGenerator
 		}
 	  }catch(IOException e){}
 	
-    stringBuffer.append(TEXT_26);
+    stringBuffer.append(TEXT_25);
     stringBuffer.append( result+"\n");
     }
-    stringBuffer.append(TEXT_27);
+    stringBuffer.append(TEXT_26);
     return stringBuffer.toString();
   }
 }
