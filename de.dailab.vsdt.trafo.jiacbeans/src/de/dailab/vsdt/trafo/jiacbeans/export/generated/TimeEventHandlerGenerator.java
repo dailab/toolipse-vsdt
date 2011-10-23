b92f02fc79825560207ddd284fc90b379993be8b
pstan <pstan@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2011-10-23 20:01:43 +0000

    git-svn-id: https://cvs.dai-labor.de/jiac/toolipse3/trunk@34414 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ de.dailab.vsdt.trafo.jiacbeans/src/de/dailab/vsdt/trafo/jiacbeans/export/generated/TimeEventHandlerGenerator.java
@@ -0,0 +1,23 @@
+package de.dailab.vsdt.trafo.jiacbeans.export.generated;
+
+public class TimeEventHandlerGenerator
+{
+  protected static String nl;
+  public static synchronized TimeEventHandlerGenerator create(String lineSeparator)
+  {
+    nl = lineSeparator;
+    TimeEventHandlerGenerator result = new TimeEventHandlerGenerator();
+    nl = null;
+    return result;
+  }
+
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "\tclass TimeEventHandler extends Thread{" + NL + "\t\tDate whenToStop;" + NL + "\t\tThread toStop;" + NL + "\t\tboolean triggered = false;" + NL + "\t\t" + NL + "\t\tpublic TimeoutEventHandler(Date then, Thread toStop){" + NL + "\t\t\twhenToStop = then;" + NL + "\t\t\tthis.toStop = toStop;" + NL + "\t\t}" + NL + "\t\tpublic void run(){" + NL + "\t\t\tlong time = then.getTime();" + NL + "\t\t\twhile(time>System.currentTimeMilis()){" + NL + "\t\t\t\ttry {" + NL + "\t\t\t\t\tThread.sleep(100);" + NL + "\t\t\t\t}catch(InterruptedException e ) { }" + NL + "\t\t\t}" + NL + "\t\t\ttoStop.stop();" + NL + "\t\t\ttriggered = true;" + NL + "\t\t}" + NL + "\t\tpublic boolean hasBeenTriggered(){" + NL + "\t\t\treturn triggered;" + NL + "\t\t}" + NL + "\t}";
+
+  public String generate(Object argument)
+  {
+    final StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append(TEXT_1);
+    return stringBuffer.toString();
+  }
+}
