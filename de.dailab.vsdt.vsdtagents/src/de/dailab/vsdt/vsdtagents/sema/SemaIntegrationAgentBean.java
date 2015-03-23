e2fdbc759d08ff9ec296ad2018994e04c15dec88
kuester <kuester@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2015-03-23 14:10:26 +0000

    finally managed to push the service into VSDT from JIAC thread
    git-svn-id: https://cvs.dai-labor.de/jiac/toolipse3/trunk@49289 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- de.dailab.vsdt.vsdtagents/src/de/dailab/vsdt/vsdtagents/sema/SemaIntegrationAgentBean.java
+++ de.dailab.vsdt.vsdtagents/src/de/dailab/vsdt/vsdtagents/sema/SemaIntegrationAgentBean.java
@@ -1,5 +1,17 @@
 package de.dailab.vsdt.vsdtagents.sema;
 
+import java.util.Collections;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -17,9 +29,7 @@ import de.dailab.vsdt.vsdtagents.Util;
 /**
  * This bean will later be used to push services from the SeMa2 into the VSDT.
  * 
- * TODO
- * zwei faelle: template oder komplette action -> 
- * zwei actions oder unterscheidung in der action?
+ * TODO add another action (or action parameter) for service-templates?
  *
  * @author kuester
  */
@@ -29,37 +39,45 @@ public class SemaIntegrationAgentBean extends AbstractMethodExposingBean {
 	public static final String ACTION_PUSH_SERVICE = BEAN_NAME + "#PushServiceIntoVSDT";
 
 	/**
-	 * TODO javadoc
+	 * Create VSDT Service object from JIAC Action or ServiceDescription 
+	 * and add it to the VSDT diagram opened in the active editor, if any.
 	 * 
 	 * @param action		the Action or ServiceDescription to add a corresponding VSDT service for
 	 * @throws Exception	exception in case anything goes wrong
 	 */
-	//FIXME if this uses 'ServiceDescription' as parameter, then it hangs, while using 'Action 'works
 	@Expose(name=ACTION_PUSH_SERVICE, scope=ActionScope.NODE)
 	public void pushServiceIntoVsdt(Action action) throws Exception {
 		
-		System.err.println("ACTION CALLED!");
+		// get BPS from editor, thus checking whether current editor is VSDT editor
+		final IWorkbenchWindow[] wbws = PlatformUI.getWorkbench().getWorkbenchWindows();
+		final IEditorPart editor = wbws[0].getActivePage().getActiveEditor();
+		final BusinessProcessSystem bps = Util.getVsdtModel(editor);
 		
-		// get BPS, thus check whether current editor is VSDT editor
-		IWorkbenchWindow[] wbws = PlatformUI.getWorkbench().getWorkbenchWindows();
-		IEditorPart editor = wbws[0].getActivePage().getActiveEditor();
-		BusinessProcessSystem bps = Util.getVsdtModel(editor);
-
 		if (bps != null) {
 
 			// create VSDT Service object corresponding to ServiceDescription
-			Service service = null;
-			if (action instanceof ServiceDescription) {
-				service = createService((ServiceDescription) action);
-			} else {
-				service = createService(action);
-			}
-			
-			/*
-			 * FIXME
-			 * java.lang.IllegalStateException: Cannot modify resource set without a write transaction
-			 */
-			bps.getServices().add(service);
+			final Service service = (action instanceof ServiceDescription)
+					? createService((ServiceDescription) action)
+					: createService(action);
+
+			// this JIAC agent is not running in the UI thread, so we have to asynch-exec this
+			Display.getDefault().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					// also, we have to put this inside a command
+					ICommandProxy command = new ICommandProxy(new AbstractTransactionalCommand(
+							TransactionUtil.getEditingDomain(bps), "Import Semantic Service", 
+							Collections.singletonList(WorkspaceSynchronizer.getFile(bps.eResource()))) {
+						@Override
+						protected CommandResult doExecuteWithResult(IProgressMonitor monitor, IAdaptable info) throws ExecutionException {
+							// finally, add the service to the BPS' list of services
+							bps.getServices().add(service);
+							return CommandResult.newOKCommandResult(bps);
+						}
+					});
+					((DiagramEditor) editor).getDiagramEditDomain().getDiagramCommandStack().execute(command);
+				}
+			});
 			
 		} else {
 			throw new Exception("Could not get Business Process System from active Editor. "
@@ -67,7 +85,14 @@ public class SemaIntegrationAgentBean extends AbstractMethodExposingBean {
 		}
 	}
 	
-	
+	/**
+	 * Create VSDT service object from given JIAC Action
+	 * 
+	 * TODO explain mapping
+	 * 
+	 * @param action	some JIAC actions
+	 * @return			according VSDT Service
+	 */
 	private Service createService(Action action) {
 		Service result = VsdtFactory.eINSTANCE.createService();
 
@@ -100,6 +125,14 @@ public class SemaIntegrationAgentBean extends AbstractMethodExposingBean {
 		return result;
 	}
 	
+	/**
+	 * Create VSDT service object from given JIAC ServiceDescription
+	 * 
+	 * TODO explain mapping
+	 * 
+	 * @param action	some JIAC ServiceDescription
+	 * @return			according VSDT Service
+	 */
 	private Service createService(ServiceDescription serviceDescription) {
 		Service result = VsdtFactory.eINSTANCE.createService();
 		
